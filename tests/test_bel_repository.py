c63474b2c934b2e44cdc398ced5e4f179ea51e92
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-07-16 16:44:20 -0400

    Update test_bel_repository.py

--- tests/test_bel_repository.py
+++ tests/test_bel_repository.py
@@ -9,9 +9,10 @@ import unittest
 from bel_repository import BELRepository
 from pybel import to_bel_path
 from pybel.examples import egf_graph
+from pybel.testing.cases import TemporaryCacheMixin
 
 
-class TestRepository(unittest.TestCase):
+class TestRepository(TemporaryCacheMixin):
     """Tests for the repository class."""
 
     def test_repository(self):
@@ -22,13 +23,18 @@ class TestRepository(unittest.TestCase):
             bel_path = os.path.join(temporary_directory, name)
             to_bel_path(egf_graph, bel_path)
 
-            r = BELRepository(temporary_directory)
-            graphs = r.get_graphs()
-
+            repository = BELRepository(temporary_directory)
+            graphs = repository.get_graphs(
+                manager=self.manager,
+                use_cached=True,
+                use_tqdm=False,
+            )
+            self.assertNotEqual(0, len(graphs), msg='No graphs returned')
+            self.assertEqual(1, len(graphs))
             self.assertIn(bel_path, graphs)
             graph = graphs[bel_path]
             self.assertEqual(graph.document, egf_graph.document)
-            self.assertEqual(set(graph.edges()), set(egf_graph.edges()))
+            self.assertEqual(set(graph.nodes()), set(egf_graph.nodes()))
             self.assertEqual(set(graph.edges()), set(egf_graph.edges()))
 
             self.assertTrue(os.path.exists(os.path.join(temporary_directory, f'{name}.json')))
