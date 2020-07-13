4cef79fa3f733f5b30f9b48bf8c5a276fd587aa4
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-07-13 16:56:39 +0200

    Code moved to main pybel repo in v0.15

--- src/bel_repository/__init__.py
+++ src/bel_repository/__init__.py
@@ -2,6 +2,5 @@
 
 """A utility for loading data from repositories of BEL documents."""
 
-from .metadata import BELMetadata  # noqa: F401
-from .repository import BELRepository  # noqa: F401
-from .version import get_version  # noqa: F401
+from pybel.repository import BELMetadata, BELRepository  # noqa: F401
+from pybel.version import get_version  # noqa: F401
--- src/bel_repository/cli.py
+++ src/bel_repository/cli.py
@@ -2,28 +2,11 @@
 
 """A command line interface for BEL repositories."""
 
-import os
-
-import click
-
-from .repository import BELRepository, append_click_group
+from pybel.repository import main
 
 __all__ = [
     'main',
 ]
-
-
-@click.group()
-@click.version_option()
-@click.option('-d', '--directory', default=os.getcwd(), type=click.Path(file_okay=False, dir_okay=True, exists=True),
-              help='Defaults to current working directory')
-@click.pass_context
-def main(ctx, directory: str):
-    """Command line interface for bel-repository."""
-    ctx.obj = BELRepository(directory=directory)
-
-
-append_click_group(main)
 
 if __name__ == '__main__':
     main()
